5bce1bd8febd897f1c13ae6b574586060c166156
Anton Morozov <antonmorozoff64@gmail.com>
2024-10-17 16:19:34 +0200

    feat(server): updated docker file and scripts

---
+++ server/scripts/docker-build.sh
@@ -0,0 +1,3 @@
+VERSION=$1
+
+docker build -t obsidian-notes-creator:$VERSION .
--- server/scripts/docker-run.sh
+++ server/scripts/docker-run.sh
@@ -1,1 +1,4 @@
-docker run -d -p 5050:5050 --env-file .env flask-app
+VERSION=$1
+OBSIDIAN_VAULT_PATH=$2
+
+docker run -d -p 5050:5050 --env-file .env -v "$OBSIDIAN_VAULT_PATH:/vault" obsidian-notes-creator:$VERSION
